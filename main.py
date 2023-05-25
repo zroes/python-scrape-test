8f1a8de4a69e0b9766b0846913d4925e0424d4c9
zroes <zk.roess@gmail.com>
2023-05-25 16:54:18 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+import requests
+import time
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+pd.set_option('display.max_colwidth', 500)
+
+quotes = []
+authors = []
+urls = [f"http://quotes.toscrape.com/page/{i}/" for i in range(1, 11)]
+for url in urls:
+    page = requests.get(url)
+    soup = bs(page.content)
+
+    quotes.extend([i.text for i in soup.find_all(class_='text')])
+    authors.extend([i.text for i in soup.find_all(class_='author')])
+
+for i in range(len(quotes)):
+    print(authors[i])
+    print(quotes[i])
